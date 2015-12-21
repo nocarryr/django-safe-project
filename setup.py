b1a701d3da2da2997f9af92cfd9a707f5a9f64dc
nocarryr <matt@nomadic-recording.com>
2015-12-21 10:43:28 -0600

    Add classifiers for PyPI

--- setup.py
+++ setup.py
@@ -20,4 +20,18 @@ setup(
     },
     setup_requires=['setuptools-markdown'],
     long_description_markdown_filename='README.md',
+    classifiers = [
+        'Development Status :: 3 - Alpha',
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Security',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+    ],
 )
