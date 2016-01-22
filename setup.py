d69865313391f68981bb47941fa9bc05ba5d7805
nocarryr <matt@nomadic-recording.com>
2016-01-22 14:04:17 -0600

    Bump version for fix in 412362d

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name = "django-safe-project",
-    version = "0.0.4",
+    version = "0.0.5",
     author = "Matthew Reid",
     author_email = "matt@nomadic-recording.com",
     description = ("Start Django projects with sensitive data outside of the "
